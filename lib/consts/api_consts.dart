180e20b6d8a5ce61111a5faa3a6f3158b0c2f5f2
TimurAsk <timuraskarovvv@gmail.com>
2023-05-08 00:29:41 +0500

    edit api

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
---
+++ lib/services/api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../consts/api_consts.dart';
+
+class ApiService {
+  static Future<void> getModels() async {
+    try {
+      var response = await http.get(Uri.parse("$BASE_URL/models"),
+          headers: {'Authorization': 'Bearer $API_KEY'});
+      Map jsonResponse = jsonDecode(response.body);
+      print('Json response $jsonResponse');
+    } catch (e) {
+      print('error $e');
+    }
+  }
+}
